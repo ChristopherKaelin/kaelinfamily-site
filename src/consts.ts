6afa65035ae8ea9e2624f7cf8e49c2f0a6973b0b
Christopher Kaelin <chriskaelin67@gmail.com>
2025-07-20 09:12:32 -0400

    Updated the title and description for the site and about me page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Kaelin Family';
+export const SITE_DESCRIPTION = 'Family stories, tech adventures, and life in Kentucky';
