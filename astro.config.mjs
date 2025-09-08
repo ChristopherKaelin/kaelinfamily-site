15a3491a84e3b3061deea6f57c832ee639a2a297
Christopher Kaelin <chriskaelin67@gmail.com>
2025-09-08 19:08:48 -0400

    updates in preparation for deployment to live site.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://your-site.com',
+  site: 'https://www.kaelinfamily.com',
   integrations: [mdx(), sitemap()],
   vite: {
     build: {
