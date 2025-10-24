9d3acf7999e85f997962fc4d96d8df9b489a328f
Christopher Kaelin <chriskaelin67@gmail.com>
2025-10-24 13:14:23 -0400

    Add Vercel adapter for SSR support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://www.kaelinfamily.com',
+  adapter: vercel({}),
 
   integrations: [mdx(), sitemap()],
   vite: {
